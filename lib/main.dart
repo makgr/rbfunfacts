6a4634c511dd50eade3ddd3e92d41ab5866ae5bb
devmak <khan@nochallenge.net>
2024-07-14 00:52:25 +0600

    install provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rbfunfacts/providers/themeProvider.dart';
 import 'package:rbfunfacts/screens/main_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (contex) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -11,9 +16,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var themeprovider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.light(useMaterial3: true),
+      theme: themeprovider.isDarkModeChecked
+          ? ThemeData.dark(useMaterial3: true)
+          : ThemeData.light(useMaterial3: true),
       home: MainScreen(),
     );
   }
